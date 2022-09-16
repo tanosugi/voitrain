385a60674ec94a647a239672306c908a644c4ac5
highlandscyclist <highlands.cyclist@gmail.com>
2022-09-16 03:55:50 +0000

    refactor editCard

--- pages/edit/[cardSetId]/index.tsx
+++ pages/edit/[cardSetId]/index.tsx
@@ -1,38 +1,18 @@
 import { Authenticator } from "@aws-amplify/ui-react";
-import { Hub } from "aws-amplify";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import Modal from "react-modal";
 import EditCardsInCardSet from "../../../src/components/EditCardsInCardSet";
 import useQueryCardSetFromId from "../../../src/hooks/useQueryCardSetFromId";
-import useQueryCardsFromCardSetId from "../../../src/hooks/useQueryCardsFromCardSetId";
 import Center from "../../../src/layout/center";
-import { Card } from "../../../src/models";
-import { CardCreate, Pluscircle } from "../../../src/ui-components";
-import CardEdit from "../../../src/ui-components/CardEdit";
-import CardListingViewCollection from "../../../src/ui-components/CardListingViewCollection";
 import CardSetDetail from "../../../src/ui-components/CardSetDetail";
 
 const Home: NextPage = () => {
   const router = useRouter();
-
   const [modalToOpen, setModalToOpen] = useState("");
   const { cardSetId } = router.query;
   const { cardSet } = useQueryCardSetFromId(cardSetId);
   const [name, setName] = useState();
-  const { cards } = useQueryCardsFromCardSetId(cardSetId);
-  const [cardToEdit, setCardToEdit] = useState();
-  Hub.listen("ui", (capsule) => {
-    if (
-      [
-        "actions:datastore:create:finished",
-        "actions:datastore:update:finished",
-      ].includes(capsule.payload.event)
-    ) {
-      setModalToOpen("");
-    }
-  });
   return (
     <>
       <Authenticator>
@@ -70,65 +50,10 @@ const Home: NextPage = () => {
                 />
               </Center>
             </Modal> */}
-          <EditCardsInCardSet cardSetId={cardSetId} />
-          <Center>
-            <Pluscircle
-              overrides={{
-                Pluscircle: {
-                  onClick: () => {
-                    setModalToOpen("CardCreate");
-                  },
-                },
-              }}
-            />
-          </Center>
-          <Modal isOpen={modalToOpen == "CardCreate"} style={customStyles}>
-            <Center>
-              <CardCreate
-                card={
-                  new Card({
-                    word: "",
-                    image_url: "",
-                    cardsetID: cardSet && cardSet?.id ? cardSet?.id : "",
-                  })
-                }
-                overrides={{
-                  close: {
-                    onClick: () => {
-                      setModalToOpen("");
-                    },
-                  },
-                }}
-              />
-            </Center>
-          </Modal>
-          <CardListingViewCollection
-            items={cards}
-            overrideItems={({ item, index }) => ({
-              overrides: {
-                pencil: {
-                  onClick: () => {
-                    setModalToOpen("CardEdit");
-                    setCardToEdit(item);
-                  },
-                },
-              },
-            })}
+          <EditCardsInCardSet
+            cardSetId={String(cardSetId)}
+            modalStyle={customStyles}
           />
-          <Modal isOpen={modalToOpen == "CardEdit"} style={customStyles}>
-            <Center>
-              <CardEdit
-                card={cardToEdit}
-                overrides={{
-                  close: {
-                    onClick: () => {
-                      setModalToOpen("");
-                    },
-                  },
-                }}
-              />
-            </Center>
-          </Modal>
         </Center>
       </Authenticator>
     </>
--- src/components/EditCardsInCardSet.tsx
+++ src/components/EditCardsInCardSet.tsx
@@ -1,9 +1,95 @@
-import { FC, ReactElement } from "react";
+import { Hub } from "aws-amplify";
+import { FC, ReactElement, useState } from "react";
+import Modal from "react-modal";
+import useQueryCardsFromCardSetId from "../hooks/useQueryCardsFromCardSetId";
+import Center from "../layout/center";
+import { Card } from "../models";
+import {
+  CardCreate,
+  CardEdit,
+  CardListingViewCollection,
+  Pluscircle,
+} from "../ui-components";
 
-const EditCardsInCardSet: FC<{ cardSetId: string }> = ({
-  cardSetId,
-}): ReactElement => {
-  return <div>EditCardsInCardSet</div>;
+const EditCardsInCardSet: FC<{
+  cardSetId: string;
+  modalStyle?: Modal.Styles | undefined;
+}> = ({ cardSetId, modalStyle }): ReactElement => {
+  const [modalToOpen, setModalToOpen] = useState("");
+  const { cards } = useQueryCardsFromCardSetId(cardSetId);
+  const [cardToEdit, setCardToEdit] = useState();
+  Hub.listen("ui", (capsule) => {
+    if (
+      [
+        "actions:datastore:create:finished",
+        "actions:datastore:update:finished",
+      ].includes(capsule.payload.event)
+    ) {
+      setModalToOpen("");
+    }
+  });
+  return (
+    <Center>
+      <Center>
+        <Pluscircle
+          overrides={{
+            Pluscircle: {
+              onClick: () => {
+                setModalToOpen("CardCreate");
+              },
+            },
+          }}
+        />
+      </Center>
+      <Modal isOpen={modalToOpen == "CardCreate"} style={modalStyle}>
+        <Center>
+          <CardCreate
+            card={
+              new Card({
+                word: "",
+                image_url: "",
+                cardsetID: cardSetId,
+              })
+            }
+            overrides={{
+              close: {
+                onClick: () => {
+                  setModalToOpen("");
+                },
+              },
+            }}
+          />
+        </Center>
+      </Modal>
+      <CardListingViewCollection
+        items={cards}
+        overrideItems={({ item, index }) => ({
+          overrides: {
+            pencil: {
+              onClick: () => {
+                setModalToOpen("CardEdit");
+                setCardToEdit(item);
+              },
+            },
+          },
+        })}
+      />
+      <Modal isOpen={modalToOpen == "CardEdit"} style={modalStyle}>
+        <Center>
+          <CardEdit
+            card={cardToEdit}
+            overrides={{
+              close: {
+                onClick: () => {
+                  setModalToOpen("");
+                },
+              },
+            }}
+          />
+        </Center>
+      </Modal>
+    </Center>
+  );
 };
 
 export default EditCardsInCardSet;
