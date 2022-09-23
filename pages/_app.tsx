1f81a6004468d2ead71ebb026a1413999a1ebe70
highlandscyclist <highlands.cyclist@gmail.com>
2022-09-23 00:31:15 +0000

    add logrocket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,24 @@ import { AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
 import { AmplifyProvider } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
+import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import config from "../src/aws-exports";
 import { studioTheme } from "../src/ui-components";
 import { GoogleAnalytics } from "../src/utils/gtag";
+
+// you can import these packages anywhere
+// const LogRocket = require('logrocket');
+// const setupLogRocketReact = require('logrocket-react');
+
+// only initialize when in the browser
+if (typeof window !== "undefined") {
+  LogRocket.init("oheadl/voitrain-main");
+  // plugins should also only be initialized when in the browser
+  setupLogRocketReact(LogRocket);
+}
 
 Amplify.configure(config);
 
